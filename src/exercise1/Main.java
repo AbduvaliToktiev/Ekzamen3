0855e72dad0569466aab5f567b389ac637bd4297
Admin <abdutokt2004>
2022-07-01 16:48:37 +0600

    Ekzamen3

---
+++ src/exercise1/Main.java
@@ -0,0 +1,21 @@
+package exercise1;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer[] mass = new Integer[100];
+        int count = 1;
+        for (int i = 0; i < mass.length; i++) {
+            mass[i] = count;
+            count++;
+            }
+            if (count-1 % 3 != 0) {
+            Set<Integer> integerSet = Arrays.stream(mass).collect(Collectors.toCollection(HashSet::new));
+            System.out.println(integerSet);
+        }
+    }
+}
--- src/exercise2/Main.java
+++ src/exercise2/Main.java
@@ -1,5 +1,7 @@
+package exercise2;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
     }
 }
